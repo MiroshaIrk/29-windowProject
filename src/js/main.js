1d13e2ab58d3f0c4545244ce69b2ac4f16d55132
MironIrk <mironkct@gmail.com>
2023-06-02 22:40:48 +0800

    form

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import './slider.js';
 import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
+import forms from './modules/forms.js';
 
 
 const init = () => {
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+  forms();
 }
 
 init();
